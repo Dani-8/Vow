46b0654b7cd2cd3af066ae39c5c99f818bbf299c
Dani-8 <daniyalw6190@gmail.com>
2026-08-03 22:37:20 +0500

    Updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,11 +6,13 @@ import { defineConfig } from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
     },
+
     server: {
       hmr: process.env.DISABLE_HMR !== 'true',
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
