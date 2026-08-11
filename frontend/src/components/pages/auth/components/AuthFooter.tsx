6354e1eb003d51517be480eca86b35074e56fc99
Dani-8 <daniyalw6190@gmail.com>
2026-08-11 17:05:17 +0500

    Updates

--- frontend/src/components/pages/auth/components/AuthFooter.tsx
+++ frontend/src/components/pages/auth/components/AuthFooter.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export const AuthFooter: React.FC = () => {
-  return (
-    <footer className="w-full max-w-6xl mx-auto text-center z-20 mt-6 pt-4 border-t border-white/40">
-      <p className="text-xs text-[#717699] font-medium">
-        © {new Date().getFullYear()} Vow App Inc. • All rights reserved.
-      </p>
-    </footer>
-  );
+    return (
+        <footer className="w-full max-w-6xl mx-auto text-center z-20 mt-6 pt-4 border-t border-white/40">
+            <p className="text-xs text-[#717699] font-medium">
+                © {new Date().getFullYear()} Vow App Inc. • All rights reserved.
+            </p>
+        </footer>
+    );
 };
