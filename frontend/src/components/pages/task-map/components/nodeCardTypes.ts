1d8d7fef3dd483cf3ce47cc1949c483bfc3070e7
Dani-8 <daniyalw6190@gmail.com>
2026-08-12 20:20:41 +0500

    Updates

---
+++ frontend/src/components/pages/task-map/components/nodeCardTypes.ts
@@ -0,0 +1,13 @@
+import { Task, SubTask } from '../../../../types';
+import { TaskMapNode } from '../types';
+
+export interface TaskNodeCardProps {
+  node: TaskMapNode;
+  task?: Task;
+  subTask?: SubTask;
+  isSelected: boolean;
+  onSelect: () => void;
+  onPositionChange: (nodeId: string, x: number, y: number) => void;
+  onDeleteNode: (nodeId: string) => void;
+  onStartConnect: (nodeId: string) => void;
+}
