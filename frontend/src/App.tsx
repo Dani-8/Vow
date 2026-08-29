5394384bf5115aa81321e501bf75b244a9db4e4e
Dani-8 <daniyalw6190@gmail.com>
2026-08-29 22:48:00 +0500

    Updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -148,6 +148,7 @@ export default function App() {
         onSubmitTask={async (data) => {
           await taskData.handleCreateOrUpdateTask(
             data,
+            modalState.editingTask?._id
           );
           modalState.setIsTaskModalOpen(false);
         }}
