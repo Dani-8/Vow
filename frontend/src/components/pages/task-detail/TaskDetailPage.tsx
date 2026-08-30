06499d59e6a5799ebb2cfb4c14509efe61dbf188
Dani-8 <daniyalw6190@gmail.com>
2026-08-30 15:50:55 +0500

    Updates

--- frontend/src/components/pages/task-detail/TaskDetailPage.tsx
+++ frontend/src/components/pages/task-detail/TaskDetailPage.tsx
@@ -248,3 +248,31 @@ export const TaskDetailPage: React.FC<TaskDetailPageProps> = ({
             )}
 
             {activeTab === 'activity' && (
+                <TaskActivityTab
+                    taskId={task._id}
+                    activities={activities}
+                    onAddComment={handleAddComment}
+                    onDeleteActivity={handleDeleteActivity}
+                />
+            )}
+
+            {/* Add / Edit Sub-Task Modal */}
+            <AddSubTaskModal
+                isOpen={isAddModalOpen}
+                onClose={() => setIsAddModalOpen(false)}
+                onSave={(newSt) => {
+                    addSubTask(newSt);
+                    addTaskActivity(task._id, {
+                        type: 'subtask_add',
+                        message: `Added new subtask: "${newSt.title}"`,
+                        user: 'Alex Rivera',
+                    });
+                    setActivities(getTaskActivities(task._id));
+                }}
+                editingSubTask={editingSubTask}
+                onUpdate={updateSubTask}
+            />
+        </div>
+    );
+};
+
