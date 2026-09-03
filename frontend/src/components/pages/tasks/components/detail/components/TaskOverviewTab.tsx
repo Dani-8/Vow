ac054a39e9cc6593b7b143ea6645fef22a907b8e
Dani-8 <daniyalw6190@gmail.com>
2026-09-03 22:40:20 +0500

    Updates

--- frontend/src/components/pages/tasks/components/detail/components/TaskOverviewTab.tsx
+++ frontend/src/components/pages/tasks/components/detail/components/TaskOverviewTab.tsx
@@ -19,3 +19,17 @@ import {
 import { Task, SubTask, TaskAttachment, TaskActivityItem, TaskStickyNote } from '../../../../../../types';
 import { TaskTabType } from './TaskDetailTabs';
 import { RotatingConsequenceBanner } from '../../../../../common/RotatingConsequenceBanner';
+
+interface TaskOverviewTabProps {
+  task: Task;
+  subTasks: SubTask[];
+  stickyNotes: TaskStickyNote[];
+  attachments: TaskAttachment[];
+  activities: TaskActivityItem[];
+  completedCount: number;
+  totalCount: number;
+  progressPercent: number;
+  onTabChange: (tab: TaskTabType) => void;
+  onToggleComplete: (task: Task) => void;
+  onEditTask?: (task: Task) => void;
+}
