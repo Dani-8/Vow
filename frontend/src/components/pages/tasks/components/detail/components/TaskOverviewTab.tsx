e4a6a1d052dc24c7759e52142802791e33057d2a
Dani-8 <daniyalw6190@gmail.com>
2026-09-03 22:40:02 +0500

    Updates

---
+++ frontend/src/components/pages/tasks/components/detail/components/TaskOverviewTab.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  CheckCircle2,
+  Clock,
+  Flame,
+  StickyNote,
+  Paperclip,
+  Activity,
+  Tag,
+  Calendar,
+  AlertCircle,
+  ExternalLink,
+  ChevronRight,
+  ListTodo,
+  Sparkles,
+  ArrowUpRight,
+  Pin
+} from 'lucide-react';
+import { Task, SubTask, TaskAttachment, TaskActivityItem, TaskStickyNote } from '../../../../../../types';
+import { TaskTabType } from './TaskDetailTabs';
+import { RotatingConsequenceBanner } from '../../../../../common/RotatingConsequenceBanner';
