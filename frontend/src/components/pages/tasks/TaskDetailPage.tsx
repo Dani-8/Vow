65dc2928fdd2adaeab7fbb4792bf3d2912d12297
Dani-8 <daniyalw6190@gmail.com>
2026-09-03 22:52:14 +0500

    Updates

---
+++ frontend/src/components/pages/tasks/TaskDetailPage.tsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { Task, SubTask, TaskAttachment, TaskActivityItem, TaskStickyNote } from '../../../types';
+import { TaskDetailHeader } from './components/detail/components/TaskDetailHeader';
+import { TaskDetailTabs, TaskTabType } from './components/detail/components/TaskDetailTabs';
+import { TaskOverviewTab } from './components/detail/components/TaskOverviewTab';
+import { TaskNotesTab } from './components/detail/components/TaskNotesTab';
+import { TaskFilesTab } from './components/detail/components/TaskFilesTab';
+import { TaskActivityTab } from './components/detail/components/TaskActivityTab';
+import { SubTaskTimeline } from './components/detail/subtasks/SubTaskTimeline';
+import { SubTaskDetailPanel } from './components/detail/subtasks/SubTaskDetailPanel';
+import { AddSubTaskModal } from './components/detail/subtasks/AddSubTaskModal';
+import { useSubTasks } from './components/detail/hooks/useSubTasks';
+import {
+    getTaskStickyNotes,
+    addTaskStickyNote,
+    updateTaskStickyNote,
+    deleteTaskStickyNote,
+    getTaskAttachments,
+    addTaskAttachment,
+    deleteTaskAttachment,
+    getTaskActivities,
+    addTaskActivity,
+    deleteTaskActivity,
+    TASK_DETAIL_UPDATED_EVENT,
+} from '../../../utils/taskDetailStorage';
