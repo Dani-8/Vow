cde0c3ec34e6fe6ea81427bacd05ba3d4b0d8576
Dani-8 <daniyalw6190@gmail.com>
2026-09-09 23:56:57 +0500

    Updates

---
+++ frontend/src/components/pages/stats/components/StatsDeepAnalytics.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { CategoryBreakdownItem, DayActivity } from '../statsHelpers';
+import { getCategoryIconComponent } from '../../../common/categoryIcons';
+import {
+    TrendingUp,
+    Compass,
+    ShieldCheck,
+    Flame,
+    BarChart3,
+    PieChart as PieIcon,
+    Layers,
+    Activity,
+} from 'lucide-react';
