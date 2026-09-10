b00dde2578e8c429ae37e624d7806183dbcf216b
Dani-8 <daniyalw6190@gmail.com>
2026-09-11 04:28:41 +0500

    Updates

---
+++ frontend/src/components/pages/stats/components/StatsExecutiveHeader.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ShieldCheck, Flame, Sparkles, Trophy } from 'lucide-react';
+import { EcosystemOverview } from '../statsHelpers';
+
+interface StatsExecutiveHeaderProps {
+    overview: EcosystemOverview;
+}
