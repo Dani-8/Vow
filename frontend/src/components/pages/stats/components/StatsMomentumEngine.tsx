4c287bff9d88f3ac76783950a92c8903663994c0
Dani-8 <daniyalw6190@gmail.com>
2026-09-12 05:47:19 +0500

    Updates

---
+++ frontend/src/components/pages/stats/components/StatsMomentumEngine.tsx
@@ -0,0 +1,20 @@
+import React, { useMemo } from 'react';
+import { DayActivity, EcosystemOverview } from '../statsHelpers';
+import {
+    TrendingUp,
+    TrendingDown,
+    Minus,
+    Zap,
+    Sparkles,
+    Flame,
+    X,
+} from 'lucide-react';
+import {
+    ResponsiveContainer,
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    Tooltip,
+    CartesianGrid,
+} from 'recharts';
