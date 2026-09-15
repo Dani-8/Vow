11cda43db59b5e774127c1af7f5c46c415946f30
Dani-8 <daniyalw6190@gmail.com>
2026-09-15 13:45:40 +0500

    Updates

---
+++ frontend/src/components/pages/stats/components/StatsDeepAnalytics.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useMemo } from 'react';
+import {
+    CategoryBreakdownItem,
+    DayActivity,
+    EcosystemOverview,
+    generateCategoryActivityHeatmap,
+} from '../statsHelpers';
+import { Task, Challenge } from '../../../../types';
+import { TaskMap } from '../../task-map/types';
+import { StatsMomentumEngine } from './StatsMomentumEngine';
+import {
+    StatsControlBar,
+    AnalyticsFilterState,
+} from './StatsControlBar';
+import { filterActivities } from './filterHelpers';
+import {
+    TrendingUp,
+    Compass,
+    PieChart as PieIcon,
+    BarChart3,
+    Activity,
+    Flame,
+    ShieldCheck,
+    X,
+    Filter,
+} from 'lucide-react';
+import {
+    Radar,
+    RadarChart,
+    PolarGrid,
+    PolarAngleAxis,
+    PolarRadiusAxis,
+    ResponsiveContainer,
+    PieChart,
+    Pie,
+    Cell,
+    Tooltip,
+    BarChart,
+    Bar,
+    LabelList,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    AreaChart,
+    Area,
+} from 'recharts';
+
